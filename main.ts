5034d62825c36c5af3d94947a070effe65055f8e
Mo1156 <58610705+Mo1156@users.noreply.github.com>
2020-02-10 14:01:12 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+let name = game.askForString("what is your name")
+let favorite_Color = game.askForString("what is your favorite color")
+let dogorcat = game.askForString("what is your favorite pet")
+ 
+game.splash("your name is :" + name )
+
+game.splash("your favorite color is :" + favorite_Color)
+
+game.splash("your favorite pet is :" + dogorcat)
+
+
